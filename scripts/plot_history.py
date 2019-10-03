ff35b8a3acd484acbb0027d34f706a01cc46e6c7
brisfors <brisfors@kth.se>
2019-10-03 16:32:00 +0200

    added acc vs valacc. May add loss

--- scripts/plot_history.py
+++ scripts/plot_history.py
@@ -12,14 +12,17 @@ plotlist = []
 
 for name in namelist:
   plotlist.insert(0, np.load(name, allow_pickle=True)['arr_0'].item()['val_acc'])
+  plotlist.insert(0, np.load(name, allow_pickle=True)['arr_0'].item()['acc'])
 
-for i in range(len(namelist)):
+for i in range(0, len(namelist), 2):
   plt.xlabel('epoch')
-  plt.ylabel('validation accuracy')
-  filename =re.search('([^/\s]+)([\s]?)+$', namelist[i]).group(1)
+  plt.ylabel('val_acc/accuracy')
+  filename =re.search('([^/\s]+)([\s]?)+$', namelist[int(i/2)]).group(1)
   filename = filename[:-11]
-  plt.title('val_acc for ' + filename)
-  plt.plot(plotlist[len(plotlist)-i -1])
+  plt.title('val_acc and acc for ' + filename)
+  plt.plot(plotlist[len(plotlist)-i -1], label='val_acc')
+  plt.plot(plotlist[len(plotlist)-i -2], label='acc')
+  plt.legend(bbox_to_anchor=(0.79,0.8), loc=2, borderaxespad=0.)
   plt.savefig('history/' + filename + '.pdf')
   plt.show()
 
